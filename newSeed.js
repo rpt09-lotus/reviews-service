f750e8986c369cc1e4d5f785a92e52b5adcff9f4
Matt Weber <matt@weberdesign-ak.com>
2019-01-22 18:38:32 -0900

    New Seed File

---
+++ newSeed.js
@@ -0,0 +1,45 @@
+const data = require('./quotes').quotes;
+const fs = require('fs');
+
+//THIS FILE GENERATES REVIEWS WHICH WERE THEN STORED IN schema.sql
+const dateGen = () => {
+  let dateStr = ''
+  const years = [2018, 2017, 2016]
+
+  let day = (Math.floor(Math.random() * (12 - 1) + 1));
+  let month = (Math.floor(Math.random() * (30 - 1) + 1));
+  let year = years[Math.floor(Math.random() * years.length)];
+
+  return dateStr += day + '/' + month + '/' + year
+}
+const seedDB = (data) => {
+  let trailId = 0;
+
+  const appendToResults = fs.createWriteStream('results.csv', {
+    flags: 'a'
+  });
+
+  appendToResults.write('user_id, trail_id, rating, description, date, act_id\n');
+
+  for (var i = 0; i < 10000000; i++) {
+    trailId++;
+    for (var j = 0; j < (Math.random() * (20 - 3) + 3); j++) {
+
+      let date = dateGen();
+      let rating = (Math.floor(Math.random() * (6 - 1) + 1));
+      let userId = (Math.floor(Math.random() * (100 - 1) + 1));
+      let review = data[Math.floor(Math.random() * data.length)];
+      let actId = (Math.floor(Math.random() * (16 - 1) + 1));
+      console.log(i);
+
+      appendToResults.write(`${userId},${trailId},${rating},${review},${date},${actId}\n`);
+    }
+  }
+  appendToResults.end();
+}
+
+seedDB(data);
+
+module.exports = {
+  seedDB
+}
