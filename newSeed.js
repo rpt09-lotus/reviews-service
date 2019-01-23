29bffd9082451551e4138088291ffc9fc11b4985
weber93 <matt@weberdesign-ak.com>
2019-01-23 04:41:30 -0900

    Still working on Seed Scriot

--- newSeed.js
+++ newSeed.js
@@ -12,34 +12,30 @@ const dateGen = () => {
 
   return dateStr += day + '/' + month + '/' + year
 }
+
 const seedDB = (data) => {
   let trailId = 0;
+  let dbData = '';
 
   const appendToResults = fs.createWriteStream('results.csv', {
     flags: 'a'
   });
 
   appendToResults.write('user_id, trail_id, rating, description, date, act_id\n');
-
+  
   for (var i = 0; i < 10000000; i++) {
     trailId++;
     for (var j = 0; j < (Math.random() * (20 - 3) + 3); j++) {
-
       let date = dateGen();
       let rating = (Math.floor(Math.random() * (6 - 1) + 1));
       let userId = (Math.floor(Math.random() * (100 - 1) + 1));
       let review = data[Math.floor(Math.random() * data.length)];
       let actId = (Math.floor(Math.random() * (16 - 1) + 1));
-      console.log(i);
 
-      appendToResults.write(`${userId},${trailId},${rating},${review},${date},${actId}\n`);
+      dbData += `${userId},${trailId},${rating},${review},${date},${actId}\n`;
     }
   }
-  appendToResults.end();
+  appendToResults.write(dbData);
 }
 
 seedDB(data);
-
-module.exports = {
-  seedDB
-}
